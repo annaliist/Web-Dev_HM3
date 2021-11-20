ac6a18dad48e1bb95e10623577956afaaf1fd5f8
annaliist <annaliis.tahevali@outlook.com>
2021-11-20 14:07:58 +0200

    added a logo to the nav

--- wd_hm3/src/App.vue
+++ wd_hm3/src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="contact">Contact</router-link>
+    <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1609072752424x654841387818197400%2Fhorisontal%2520%25E2%2580%2593%2520koopia.jpg" alt="">
+    
+    <div class="links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="contact">Contact</router-link>
+    </div>
   </div>
   
   <router-view />
@@ -29,16 +33,25 @@
 }
 
 #nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
   padding: 20px;
 
   a {
     font-weight: bold;
     color: #2c3e50;
     font-size: 20px;
+    padding: 0px 5px 0px 5px;
 
     &.router-link-exact-active {
-      color: #666666;
+      color: #EFEFEF;
     }
+  }
+
+  img {
+    height: 40px;
+    width: auto;
   }
 }
 
