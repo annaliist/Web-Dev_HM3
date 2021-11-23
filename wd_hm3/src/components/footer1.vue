da8d580041c3ac0ffe0b7eb267755d64b0e207b6
annaliist <annaliis.tahevali@outlook.com>
2021-11-23 10:18:45 +0200

    components had not been pushed for some reason, adding now

---
+++ wd_hm3/src/components/footer1.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="footer1">
+    <router-link to="contact">Contact Us</router-link>
+
+    <div class="icons">
+      <img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png"/>
+      <img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/>
+      <img src="https://img.icons8.com/ios/50/000000/mail.png"/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Footer1",
+}
+</script>
+
+<style scoped>
+#footer1 {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  padding: 20px;
+  align-content: space-around;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    font-size: 20px;
+  }
+
+  .icons{
+    img {
+      padding-left: 10px;
+      padding-right: 10px;
+    }
+  }
+}
+</style>
---
+++ wd_hm3/src/components/navbar.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="navbar">
+    <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1609072752424x654841387818197400%2Fhorisontal%2520%25E2%2580%2593%2520koopia.jpg?w=256&h=45&auto=compress&fit=crop&dpr=1" alt="">
+    
+    <div class="links">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="contact">Contact</router-link>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "navigation",
+};
+</script>
+
+
+<style scoped>
+#navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    font-size: 20px;
+    padding: 0px 5px 0px 5px;
+
+    &.router-link-exact-active {
+      color: #EFEFEF;
+    }
+  }
+
+  img {
+    height: 40px;
+    width: auto;
+  }
+}
+</style>
